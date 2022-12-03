a2e83f10f305e6cd9c85305a6d5c1b51421dbdbc
MaxVanVort <119711805+MaxVanVort@users.noreply.github.com>
2022-12-02 23:25:57 -0500

    Completed ArrayList

--- Project5/Project5.java
+++ Project5/Project5.java
@@ -8,22 +8,53 @@ public class Project5
     private String date;
     private int tempurature;
     private WeatherSystem forecast;
-    private String message;
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to the weather tracker for the month of December!"+"\n");
-        System.out.println("What day of the month would you like the weather report for? Please submit a number between 1 and 31, such as '17' or '25'.");
+        System.out.println("Welcome to the December Holiday Weather Tracker!"+"\n");
+        System.out.println("Which day in the December Holidy Season (Dec 15-31) would you like to know the weather of? Please input a number, such as 15 or 25.");
         int UserDay=input.nextInt();
     }
-    public Project5(String d, WeatherSystem s, int t, String m){
+    public Project5(String d, int t, WeatherSystem s){
         this.date=d;
         this.tempurature=t;
         this.forecast=s;
-        this.message=m;
     }
     public static void weatherForDecember(ArrayList<Project5> list)
     {
-        
+        Project5 Dec15= new Project5("December 15th", 40, Project5.WeatherSystem.WINDY);
+        list.add(Dec15);
+        Project5 Dec16=new Project5("December 16th", 39, Project5.WeatherSystem.SUNNY);
+        list.add(Dec16);
+        Project5 Dec17=new Project5("December 17th", 38, Project5.WeatherSystem.WINDY);
+        list.add(Dec17);
+        Project5 Dec18=new Project5("December 18th", 38, Project5.WeatherSystem.WINDY);
+        list.add(Dec18);
+        Project5 Dec19=new Project5("December 19th", 37, Project5.WeatherSystem.CLOUDY);
+        list.add(Dec19);
+        Project5 Dec20=new Project5("December 20th", 33, Project5.WeatherSystem.SUNNY);
+        list.add(Dec20);
+        Project5 Dec21=new Project5("December 21st", 34, Project5.WeatherSystem.SUNNY);
+        list.add(Dec21);
+        Project5 Dec22=new Project5("December 22nd", 32, Project5.WeatherSystem.WINDY);
+        list.add(Dec22);
+        Project5 Dec23=new Project5("December 23rd", 31, Project5.WeatherSystem.CLOUDY);
+        list.add(Dec23);
+        Project5 Dec24=new Project5("December 24th", 32, Project5.WeatherSystem.SNOWSTORM);
+        list.add(Dec24);
+        Project5 Dec25=new Project5("December 25th", 31, Project5.WeatherSystem.SUNNY);
+        list.add(Dec25);
+        Project5 Dec26=new Project5("December 26th", 32, Project5.WeatherSystem.WINDY);
+        list.add(Dec26);
+        Project5 Dec27=new Project5("December 27th", 29, Project5.WeatherSystem.CLOUDY);
+        list.add(Dec27);
+        Project5 Dec28=new Project5("December 28th", 30, Project5.WeatherSystem.CLOUDY);
+        list.add(Dec28);
+        Project5 Dec29=new Project5("December 29th", 29, Project5.WeatherSystem.BLIZZARD);
+        list.add(Dec29);
+        Project5 Dec30=new Project5("December 30th", 32, Project5.WeatherSystem.SUNNY);
+        list.add(Dec30);
+        Project5 Dec31=new Project5("December 31st", 31, Project5.WeatherSystem.WINDY);
+        list.add(Dec31);
     }
 }
